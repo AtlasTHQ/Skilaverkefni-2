b377dfd1540f2479e1cfd8413292a0da8978e501
AtlasTHQ <atlasthq@gmail.com>
2016-05-06 10:30:14 +0000

    Kláraði api + chart

--- js/js.js
+++ js/js.js
@@ -34,66 +34,74 @@ $(document).ready(function(){
         'success': function (data) {
             localJson = data;
             console.log(localJson);
+
+            $(document).on('change', 'select', function () {
+                console.log($(this).val()); // the selected options’s value
+                // if you want to do stuff based on the OPTION element:
+                var opt = $(this).find('option:selected')[0];
+                console.log(opt);
+                switch (opt) {
+                    case 1:
+                        console.log("Evra!");
+                        break;
+                    case 2:
+                        console.log("USD!");
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    case 14:
+                        break;
+                    case 15:
+                        break;
+                    case 16:
+                        break;
+                    case 17:
+                        break;
+                    case 18:
+                        break;
+                }
+                function loadChart (parameter){
+                    console.log(parameter);
+                    usableOpt = parameter;
+                    for (i = 0; i < localJson.results[0].lastValues.length; i++){
+                        localJson.results[usableOpt].lastValues[i] + ","
+                    }
+                }
+                var chart = c3.generate({
+                    bindto: '#chart',
+                    data: {
+                        columns: [
+                            //['ISK', 30, 200, 100, 400, 150, 250],
+                            [loadChart(opt)]
+                        ]
+                    }
+                });
+            })
         }
     });
-    $(document).on('change', 'select', function() {
-        console.log($(this).val()); // the selected options’s value
-        // if you want to do stuff based on the OPTION element:
-        var opt = $(this).find('option:selected')[0];
-        console.log(opt);
-        switch (opt){
-            case 1:
-                console.log("Evra!");
-                break;
-            case 2:
-                console.log("USD!");
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-            case 9:
-                break;
-            case 10:
-                break;
-            case 11:
-                break;
-            case 12:
-                break;
-            case 13:
-                break;
-            case 14:
-                break;
-            case 15:
-                break;
-            case 16:
-                break;
-            case 17:
-                break;
-            case 18:
-                break;
-        }
-        var chart = c3.generate({
-            bindto: '#chart',
-            data: {
-                columns: [
-                    ['ISK', 30, 200, 100, 400, 150, 250],
-                    [$(opt).val(), 50, 20, 10, 40, 15, 25]
-                ]
-            }
-        });
     });
 
     $("button").click(function(){
         $("p").toggle();
     });
-});
 
--- js/script.js
+++ js/script.js
@@ -1,3 +1,125 @@
-/**
- * Created by 1612972679 on 10.3.2016.
- */
+$(document).ready(function() {
+    model = {
+        localJson: {},
+        currency: 0,
+        valueHolder: [],
+        selectedCurrency: 0
+    };
+
+    controller = {
+        init: function () {
+            $.getJSON("js/api.json", function (data) {
+                model.localJson = data;
+            }).done(function () {
+                console.log(model.localJson)
+                view.init()
+            });
+            $("button").click(function(){
+                $("p").toggle();
+            });
+        },
+    };
+
+    view = {
+        init: function () {
+            var template = $("#upperContainer").html();
+            console.log(template);
+            //Compile-a það
+            var renderer = Handlebars.compile(template);
+            //Og svo loks render-a og skila því inn í html-ið
+            var result = renderer(model.localJson);
+            $("#upperContainer").html(result);
+            var template = $("#infoBlockText").html();
+            //Compile-a það
+            var renderer = Handlebars.compile(template);
+            //Og svo loks render-a og skila því inn í html-ið
+            var result = renderer(model.localJson);
+            $("#infoBlockText").html(result);
+            $.each(model.localJson.results, function()
+            {
+                console.log("XXX");
+            });
+            console.log(model.localJson.results);
+            $(document).on('change', 'select', function(){
+                console.log($(this).val()); // the selected options’s value
+                var chartOpt = $(this).find('option:selected')[0];
+                opt = $(this).val();
+                switch (opt) {
+                    case "EUR":
+                        selectedCurrency = 0;
+                        break;
+                    case "USD":
+                        selectedCurrency = 1;
+                        break;
+                    case "GBP":
+                        selectedCurrency = 2;
+                        break;
+                    case "JPY":
+                        selectedCurrency = 3;
+                        break;
+                    case "NOK":
+                        selectedCurrency = 4;
+                        break;
+                    case "SEK":
+                        selectedCurrency = 5;
+                        break;
+                    case "DKK":
+                        selectedCurrency = 6;
+                        break;
+                    case "CHF":
+                        selectedCurrency = 7;
+                        break;
+                    case "CAD":
+                        selectedCurrency = 8;
+                        break;
+                    case "AUD":
+                        selectedCurrency = 9;
+                        break;
+                    case "ZAR":
+                        selectedCurrency = 10;
+                        break;
+                    case "HKD":
+                        selectedCurrency = 11;
+                        break;
+                    case "NZD":
+                        selectedCurrency = 12;
+                        break;
+                    case "XDR":
+                        selectedCurrency = 13;
+                        break;
+                    case "PLN":
+                        selectedCurrency = 14;
+                        break;
+                    case "SGD":
+                        selectedCurrency = 15;
+                        break;
+                    case "CNY":
+                        selectedCurrency = 16;
+                        break;
+                    case "GVT":
+                        selectedCurrency = 17;
+                        break;
+                }
+                function loadChart (parameter){
+                    console.log();
+                    for (i = 0; i < model.localJson.results[parameter].lastValues.length; i++){
+
+                        model.valueHolder[i+1] = model.localJson.results[parameter].lastValues[i];
+                    }
+                    model.valueHolder[0] = opt + " to ISK";
+                }
+                loadChart(selectedCurrency);
+                var chart = c3.generate({
+                    bindto: '#chart',
+                    data: {
+                        columns: [
+                            //['ISK', 30, 200, 100, 400, 150, 250],
+                            model.valueHolder
+                        ]
+                    }
+                });
+            });
+        }
+    };
+    controller.init();
+});
